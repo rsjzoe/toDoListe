3e7542a6b5a4a01ce42ce4cab55fb154e81d21d3
rsjzoe <rsjzoe@gmail.com>
2024-03-23 14:42:52 +0300

    edite effacer

--- front/src/main.js
+++ front/src/main.js
@@ -5,26 +5,38 @@ const addButton = document.querySelector(".btn-add");
 const btnClose = document.querySelector(".btn-close");
 const tbody = document.querySelector("tbody");
 const btnCreate = document.querySelector(".btn-create");
+const btnUptdate = document.querySelector(".btn-update");
 const nameInput = document.querySelector(".name-input");
 const jobInput = document.querySelector(".job-input");
-const removeData = document.querySelector(`${removeAction}`);
-const dataTable = [
-  { name: "Koto", job: "manager" },
-  { name: "rasoa", job: "dev" },
+
+let dataTable = [
+  { id: 1, name: "Koto", job: "manager" },
+  { id: 2, name: "rasoa", job: "dev" },
 ];
+let compteur = 3;
 
-addButton.addEventListener("click", () => {
+function showModal() {
   modal.style.display = "grid";
-});
-btnClose.addEventListener("click", () => {
+}
+function closeModal() {
   modal.style.display = "none";
+}
+addButton.addEventListener("click", () => {
+  showModal();
+  btnUptdate.style.display = "none";
+  btnCreate.style.display = "block";
 });
 
-function insertData(name, job) {
+btnClose.addEventListener("click", () => {
+  closeModal();
+});
+
+/*---INSERT_DATA---*/
+function insertData(name, job, id) {
   tbody.insertAdjacentHTML(
     "beforeend",
     `
-        <tr class="even:bg-blue-gray-50/50">
+        <tr class="${"tr-" + id} even:bg-blue-gray-50/50">
                   <td class="p-4">
                     <p
                       class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900"
@@ -49,38 +61,82 @@ function insertData(name, job) {
                   <td class="p-4 flex gap-7">
                     <a
                       href="#"
-                      class="block font-sans text-sm bg-green-100 w-11 text-center rounded-sm text-green-700 antialiased font-medium leading-normal text-blue-gray-900"
+                      class="${
+                        "e-" + id
+                      } block font-sans text-sm bg-green-100 w-11 text-center rounded-sm text-green-700 antialiased font-medium leading-normal text-blue-gray-900"
                       >Edit</a
                     >
                     <a
                       href="#"
-                      class="${removeAction} block font-sans text-sm antialiased w-16 text-center rounded-sm bg-red-100 font-medium leading-normal text-red-700"
-                      >remove</a
+                      class="block font-sans text-sm antialiased w-16 text-center rounded-sm bg-red-100 font-medium leading-normal text-red-700"
+                      id="${"rmv" + id}" >remove</a
                     >
                   </td>
         </tr>`
   );
+  removeData(id);
+  editeTable(id);
 }
+function renderData() {
+  tbody.innerHTML = "";
+  for (let indexData of dataTable) {
+    insertData(indexData.name, indexData.job, indexData.id);
+  }
+}
+renderData();
 
-for (let indexData of dataTable) {
-  insertData(indexData.name, indexData.job);
-}
+/*---ADD_DATA---*/
 btnCreate.addEventListener("click", () => {
   modal.style.display = "none";
-  insertData(nameInput.value, jobInput.value);
-  dataTable.push({ name: nameInput.value, job: jobInput.value });
-  console.log(dataTable);
+  insertData(nameInput.value, jobInput.value, compteur);
+  dataTable.push({ name: nameInput.value, job: jobInput.value, id: compteur });
+  compteur++;
   nameInput.value = "";
   jobInput.value = "";
 });
-console.log(removeData);
 
+/*--remove-----*/
+function removeData(id) {
+  document.querySelector("#" + "rmv" + id).addEventListener("click", () => {
+    document.querySelector("." + "tr-" + id).remove();
+    dataTable = effacer(id);
+  });
+}
 
+function effacer(idVal) {
+  let tab = [];
+  for (let index of dataTable) {
+    if (index.id !== idVal) {
+      tab.push(index);
+    }
+  }
+  return tab;
+}
 
-// function remove(removeAction){
-//   document.getElementsByClassName(`${removeAction}`).remove()
-// }
+/*---edite----*/
+let currentId = 0
+function editeTable(id) {
+  document.querySelector("." + "e-" + id).addEventListener("click", () => {
+    showModal();
+    btnCreate.style.display = "none";
+    btnUptdate.style.display = "block";
+    for (let indexDataTab of dataTable) {
+      if (indexDataTab.id == id) {
+        nameInput.value = indexDataTab.name;
+        jobInput.value = indexDataTab.job;
+      }
+    }
+    currentId = id
+  });
+}
 
-// removeData.addEventListener('click', ()=>{
-//   removeDataTable(dataTable)
-// })
+btnUptdate.addEventListener("click", () => {
+  for (let index of dataTable) {
+    if (index.id == currentId) {
+      index.name = nameInput.value;
+      index.job = jobInput.value;
+    }
+  }
+  closeModal();
+  renderData();
+});
